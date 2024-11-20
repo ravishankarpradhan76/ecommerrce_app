ab39b7a737c0ce3323e104d096d51fbebb6c701b
Ravi Shankar Pradhan <ravishankarpradhan76@gmail.com>
2024-11-20 23:21:34 +0530

    ecommerce

--- lib/Pages/Profile_Page.dart
+++ lib/Pages/Profile_Page.dart
@@ -13,53 +13,27 @@ class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar:AppBar(
-          backgroundColor: Colors.teal[200],
-          title: Text('E-commerce'),
-          toolbarHeight: 80,
-          actions: [
-            Padding(
-              padding: const EdgeInsets.only(right: 12),
-              child: Row(
-                children: [
-                  Icon(Icons.settings_outlined),
-                  SizedBox(width: 20),
-                  Icon(Icons.notifications_none),
-                  SizedBox(width: 20),
-                  Icon(Icons.search),
-                ],
+      appBar: AppBar(
+        title: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Container(
+            height: 56, // Adjust the height to fit inside AppBar
+            width: double.infinity, // Take full width
+            color: Colors.teal,
+            child: Center( // Centering the text inside the Container
+              child: Text(
+                'Ravi Shankar Pradhan',
+                style: TextStyle(
+                  fontSize: 18, // Adjust font size for better appearance
+                  color: Colors.white, // Text color
+                ),
               ),
             ),
-          ],
+          ),
         ),
-      body: Column(
-        children: [
-          Row(
-            children: [
-              CircleAvatar(
-                radius: 17,
-                backgroundColor: Colors.black12,
-                child: Center(child: Icon(Icons.person_outline_outlined,color: Colors.white60,)),
-              ),
-              SizedBox(width: 5,),
-              Text('Hello, ravi',style: TextStyle(fontSize: 16),),
-              Icon(Icons.keyboard_arrow_down),
-            ],
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 20),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                RoundOption(title: 'Orders',),
-                RoundOption(title: 'Lists',),
-                RoundOption(title: 'Account',),
-                RoundOption(title: 'Buy Again',),
-              ],
-            ),
-          ),
-        ],
+        backgroundColor: Colors.teal[200],
       ),
+
     );
   }
 }
--- lib/Pages/menu_page.dart
+++ lib/Pages/menu_page.dart
@@ -10,13 +10,12 @@ class MenuPage extends StatefulWidget {
 }
 
 class _MenuPageState extends State<MenuPage> {
-  // List to hold product data
   List<dynamic> products = [];
 
   Future<void> getUserApi() async {
-  final response = await http.get(Uri.parse('https://api.escuelajs.co/api/v1/products'));
+    final response =
+        await http.get(Uri.parse('https://api.escuelajs.co/api/v1/products'));
     if (response.statusCode == 200) {
-      // Decode and assign the data to the products list
       setState(() {
         products = jsonDecode(response.body.toString());
       });
@@ -28,7 +27,7 @@ class _MenuPageState extends State<MenuPage> {
   @override
   void initState() {
     super.initState();
-    getUserApi();  // Fetch data when the page is initialized
+    getUserApi();
   }
 
   @override
@@ -37,53 +36,339 @@ class _MenuPageState extends State<MenuPage> {
       appBar: AppBar(
         title: Padding(
           padding: const EdgeInsets.all(8.0),
-          child: TextFormField(
-            decoration: InputDecoration(
-              hintText: 'Search or ask a questi...',
+          child: Container(
+            height: 43,
+            child: TextFormField(
+              decoration: InputDecoration(
                 prefixIcon: Icon(Icons.search),
-                border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(10)
+                hintText: 'Search or ask a question...',
+                hintStyle: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w400,
                 ),
-              filled: true,
-              fillColor: Colors.white
+                suffixIcon: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    IconButton(
+                      icon: Icon(Icons.camera_alt_outlined),
+                      onPressed: () {},
+                    ),
+                    IconButton(
+                      icon: Icon(Icons.mic),
+                      onPressed: () {},
+                    ),
+                  ],
+                ),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                filled: true,
+                fillColor: Colors.white,
+              ),
             ),
           ),
         ),
-        actions: [ Icon(Icons.qr_code_2_sharp),],
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 12),
+            child: Icon(Icons.qr_code_2_sharp),
+          ),
+        ],
         backgroundColor: Colors.teal[200],
-
       ),
-      body: products.isEmpty?
-      Center(child: CircularProgressIndicator()):
-      Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: GridView.builder( gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2, // 2 items per row
-            crossAxisSpacing: 5, // Spacing between columns
-            mainAxisSpacing: 5, // Spacing between rows
-            childAspectRatio: 1,),
-          itemCount: products.length,
-          itemBuilder: (context, index) {
-            var product = products[index];
-            return Card(
-                elevation: 1,
-                shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10), // Rounded corners
-            side: BorderSide(
-            color: Colors.teal, // Outline color
-            width: 1, // Outline width
-            ),
-            ),
-
-              child: ListTile(
-                title: Column(
+      body: products.isEmpty
+          ? Center(child: CircularProgressIndicator())
+          : SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
                   children: [
-                    Text(product['title'].toString()),
+                    GridView.builder(
+                      shrinkWrap:
+                          true, // Ensure grid does not take up too much space
+                      physics:
+                          NeverScrollableScrollPhysics(), // Prevents grid from scrolling
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2,
+                        crossAxisSpacing: 5,
+                        mainAxisSpacing: 5,
+                        childAspectRatio: 1,
+                      ),
+                      itemCount: products.length,
+                      itemBuilder: (context, index) {
+                        var product = products[index];
+                        return Card(
+                          elevation: 1,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            side: BorderSide(
+                              color: Colors.teal,
+                              width: 1,
+                            ),
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Column(
+                              children: [
+                                Text(
+                                  product['title'].toString(),
+                                  maxLines: 2,
+                                  overflow: TextOverflow.ellipsis,
+                                  style: TextStyle(
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.w500),
+                                ),
+                                SizedBox(height: 8),
+                                Image(
+                                  image: NetworkImage(product['images'][0]),
+                                  height: 80,
+                                  width: 90,
+                                ),
+                              ],
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                    // Display the TextFormField only after the last product
+                    if (products.isNotEmpty)
+                      Column(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(4.0),
+                            child: Container(
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(8),
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: Colors.black
+                                        .withOpacity(0.2), // Shadow color
+                                    spreadRadius: 3,
+                                    blurRadius: 5,
+                                    offset:
+                                        Offset(-1, 1), // Shadow position (x, y)
+                                  ),
+                                ],
+                              ),
+                              height: 48,
+                              child: TextFormField(
+                                decoration: InputDecoration(
+                                  hintText: 'Switch Accounts',
+                                  hintStyle: TextStyle(
+                                      fontSize: 17,
+                                      fontWeight: FontWeight.w400),
+                                  suffixIcon: Icon(Icons.chevron_right_rounded),
+                                  border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(8),
+                                  ),
+                                  filled: true,
+                                  fillColor: Colors.white70,
+                                  enabledBorder: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(8),
+                                    borderSide: BorderSide(
+                                      color: Colors
+                                          .teal, // Border color when the field is not focused
+                                      width: 1,
+                                    ),
+                                  ),
+                                  // Border color when the field is focused
+                                  focusedBorder: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(8),
+                                    borderSide: BorderSide(
+                                      color: Colors
+                                          .blue, // Border color when focused
+                                      width: 2.0,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(4.0),
+                            child: Container(
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(8),
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: Colors.black
+                                        .withOpacity(0.2), // Shadow color
+                                    spreadRadius: 3,
+                                    blurRadius: 5,
+                                    offset:
+                                        Offset(-1, 1), // Shadow position (x, y)
+                                  ),
+                                ],
+                              ),
+                              height: 48,
+                              child: TextFormField(
+                                decoration: InputDecoration(
+                                  hintText: 'Sign Out',
+                                  hintStyle: TextStyle(
+                                      fontSize: 17,
+                                      fontWeight: FontWeight.w400),
+                                  suffixIcon: Icon(Icons.chevron_right_rounded),
+                                  border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(8),
+                                  ),
+                                  filled: true,
+                                  fillColor: Colors.white70,
+                                  enabledBorder: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(8),
+                                    borderSide: BorderSide(
+                                      color: Colors
+                                          .teal, // Border color when the field is not focused
+                                      width: 1,
+                                    ),
+                                  ),
+                                  // Border color when the field is focused
+                                  focusedBorder: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(8),
+                                    borderSide: BorderSide(
+                                      color: Colors
+                                          .blue, // Border color when focused
+                                      width: 2.0,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(4.0),
+                            child: Container(
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(8),
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: Colors.black
+                                        .withOpacity(0.2), // Shadow color
+                                    spreadRadius: 3,
+                                    blurRadius: 5,
+                                    offset:
+                                        Offset(-1, 1), // Shadow position (x, y)
+                                  ),
+                                ],
+                              ),
+                              height: 48,
+                              child: TextFormField(
+                                decoration: InputDecoration(
+                                  hintText: 'Customer Service',
+                                  hintStyle: TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.w400),
+                                  suffixIcon: Icon(Icons.chevron_right_rounded),
+                                  border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(8),
+                                  ),
+                                  filled: true,
+                                  fillColor: Colors.white70,
+                                  enabledBorder: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(8),
+                                    borderSide: BorderSide(
+                                      color: Colors
+                                          .teal, // Border color when the field is not focused
+                                      width: 1,
+                                    ),
+                                  ),
+                                  // Border color when the field is focused
+                                  focusedBorder: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(8),
+                                    borderSide: BorderSide(
+                                      color: Colors
+                                          .blue, // Border color when focused
+                                      width: 2.0,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 13,bottom: 90),
+                            child: Row(
+                              children: [
+                                Icon(
+                                  Icons.info,
+                                  color: Colors.teal[300],
+                                  size: 25,
+                                ),
+                                SizedBox(
+                                  width: 8,
+                                ),
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      'Looking for the app settings? They have moved',style: TextStyle(
+                                          fontSize: 14, fontWeight: FontWeight.w400),
+                                    ),
+                                    Row(
+                                      children: [
+                                        Text(
+                                          'to',style: TextStyle(
+                                            fontSize: 14, fontWeight: FontWeight.w400),
+                                        ),
+                                        Icon(Icons.lock_outline,size: 17,)
+                                      ],
+                                    ),
+                                  ],
+                                )
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
                   ],
                 ),
               ),
-            );
-          },
+            ),
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(12),
+            topRight: Radius.circular(12),
+          ),
+          color: Colors.white,
+        ),
+        height: 75,
+        width: double.infinity,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 9),
+          child: Row(
+            children: [
+              _buildBottomNavItem('Orders'),
+              SizedBox(width: 9),
+              _buildBottomNavItem('Buy Again'),
+              SizedBox(width: 9),
+              _buildBottomNavItem('Account'),
+              SizedBox(width: 9),
+              _buildBottomNavItem('Lists'),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildBottomNavItem(String label) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        border: Border.all(
+          color: Colors.black12,
+          width: 1,
+        ),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+        child: Text(
+          label,
+          style: TextStyle(
+            fontSize: 15,
+            color: Colors.black87,
+            fontWeight: FontWeight.w400,
+          ),
         ),
       ),
     );
